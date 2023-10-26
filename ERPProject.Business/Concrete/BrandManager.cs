eef18a73d619c970ac102149dfe03789fe272fd9
Emre Kaya <emrekaya2501@gmail.com>
2023-10-26 13:50:29 +0300

    emre

--- ERPProject.Business/Concrete/BrandManager.cs
+++ ERPProject.Business/Concrete/BrandManager.cs
@@ -10,7 +10,7 @@ using System.Threading.Tasks;
 
 namespace ERPProject.Business.Concrete
 {
-    internal class BrandManager : IBrandService
+    public class BrandManager : IBrandService
     {
         private readonly IUnitOfWork _uow;
 
@@ -26,14 +26,14 @@ namespace ERPProject.Business.Concrete
             return Entity;
         }
 
-        public Task<IEnumerable<Brand>> GetAllAsync(Expression<Func<Brand, bool>> Filter = null, params string[] IncludeProperties)
+        public async Task<IEnumerable<Brand>> GetAllAsync(Expression<Func<Brand, bool>> Filter = null, params string[] IncludeProperties)
         {
-            return _uow.BrandRepository.GetAllAsync(Filter, IncludeProperties);
+            return await _uow.BrandRepository.GetAllAsync(Filter, IncludeProperties);
         }
 
-        public Task<Brand> GetAsync(Expression<Func<Brand, bool>> Filter, params string[] IncludeProperties)
+        public async Task<Brand> GetAsync(Expression<Func<Brand, bool>> Filter, params string[] IncludeProperties)
         {
-            return _uow.BrandRepository.GetAsync(Filter, IncludeProperties);
+            return await _uow.BrandRepository.GetAsync(Filter, IncludeProperties);
         }
 
         public void Remove(Brand Entity)
