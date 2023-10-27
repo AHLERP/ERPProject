23e4dde59565e61fee3259addbaf0c7402113923
Hakan <hakancyln19@gmail.com>
2023-10-27 13:17:40 +0300

    User area  Controller oluşturuldu

---
+++ ERPProject.UI/Areas/User/Controllers/CompanyController.cs
@@ -0,0 +1,63 @@
+﻿using ERPProject.Entity.DTO.CompanyDTO;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ERPProject.UI.Areas.User.Controllers
+{
+    public class CompanyController : BaseController
+    {
+        private readonly string url = "";
+        public CompanyController(IHttpClientFactory httpClientFactory) : base(httpClientFactory)
+        {
+
+        }
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var val = await GetAllAsync<CompanyDTOResponse>(url);
+            return View(val);
+        }
+        [HttpGet]
+        public async Task<IActionResult> Get(long id)
+        {
+            var val = await GetAsync<CompanyDTOResponse>(url + id);
+            return View(val);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Add(CompanyDTORequest p)
+        {
+            var response = await AddAsync(p, url);
+            if (response)
+            {
+                return RedirectToAction("Index", "Company");
+
+            }
+            return RedirectToAction("Index", "Home");
+
+        }
+        [HttpPost]
+        public async Task<IActionResult> Update(CompanyDTORequest p)
+        {
+            var response = await UpdateAsync(p, url);
+            if (response)
+            {
+                return RedirectToAction("Index", "Company");
+
+            }
+            return RedirectToAction("Index", "Home");
+
+        }
+        [HttpPost]
+        public async Task<IActionResult> Delete(long id)
+        {
+            var response = await DeleteAsync(url+id);
+            if (response)
+            {
+                return RedirectToAction("Index", "Company");
+
+            }
+            return RedirectToAction("Index", "Home");
+
+        }
+
+    }
+}
---
+++ ERPProject.UI/Areas/User/Controllers/UserRoleController.cs
@@ -0,0 +1,62 @@
+﻿using ERPProject.Entity.DTO.UserRoleDTO;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ERPProject.UI.Areas.User.Controllers
+{
+    public class UserRoleController : BaseController
+    {
+        private readonly string url = "";
+        public UserRoleController(IHttpClientFactory httpClientFactory) : base(httpClientFactory)
+        {
+
+        }
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var val = await GetAllAsync<UserRoleDTOResponse>(url);
+            return View(val);
+        }
+        [HttpGet]
+        public async Task<IActionResult> Get(long id)
+        {
+            var val = await GetAsync<UserRoleDTOResponse>(url + id);
+            return View(val);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Add(UserRoleDTORequest p)
+        {
+            var response = await AddAsync(p, url);
+            if (response)
+            {
+                return RedirectToAction("Index", "UserRole");
+
+            }
+            return RedirectToAction("Index", "Home");
+
+        }
+        [HttpPost]
+        public async Task<IActionResult> Update(UserRoleDTORequest p)
+        {
+            var response = await UpdateAsync(p, url);
+            if (response)
+            {
+                return RedirectToAction("Index", "UserRole");
+
+            }
+            return RedirectToAction("Index", "Home");
+
+        }
+        [HttpPost]
+        public async Task<IActionResult> Delete(long id)
+        {
+            var response = await DeleteAsync(url + id);
+            if (response)
+            {
+                return RedirectToAction("Index", "UserRole");
+
+            }
+            return RedirectToAction("Index", "Home");
+
+        }
+    }
+}
