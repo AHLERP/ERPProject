ec27d8c268925b8a35b08bc21a56eb0c96ecfca6
enesozsecer <ensozscr@gmail.com>
2023-10-27 08:13:23 +0300

    UI oluşturuldu.

---
+++ ERPProject.UI/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ERPProject.UI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class UserController : Controller
+    {
+        #region Defines
+        private readonly HttpClient _httpClient;
+        private string url = "url yazılacak";
+        #endregion
+        #region Constructor
+        public UserController(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+        #endregion
+        [HttpGet("/Admin/Kullanicilar")]
+        public async Task<IActionResult> Index()
+        {
+
+            return View();
+        }
+    }
+}
