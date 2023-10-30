ec251379e1cb5d18c46432aa3985cc62783c4a1b
Hakan <hakancyln19@gmail.com>
2023-10-30 10:15:55 +0300

    ProductVM oluştu

---
+++ ERPProject.UI/Areas/Admin/Models/Product/ProductVM.cs
@@ -0,0 +1,22 @@
+﻿using ERPProject.Entity.DTO.BrandDTO;
+using ERPProject.Entity.DTO.CategoryDTO;
+using ERPProject.Entity.Poco;
+
+namespace ERPProject.UI.Areas.Admin.Models.Product
+{
+    public class ProductVM
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string? Description { get; set; }
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+        public int BrandId { get; set; }
+        public string BrandName { get; set; }
+
+
+        public virtual ICollection<BrandDTOResponse> Brands { get; set; } = new List<BrandDTOResponse>();
+
+        public virtual ICollection<CategoryDTOResponse> Categories { get; set; } = new List<CategoryDTOResponse>();
+    }
+}
