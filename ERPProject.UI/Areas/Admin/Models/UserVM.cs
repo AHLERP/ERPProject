4c2846ea2ecfd70425b42a68f675feb6a75841d3
Hakan <hakancyln19@gmail.com>
2023-10-30 08:51:21 +0300

    UserVM oluşturuldu içerisinde bulunan Role ilişkisini dto ile değiştir

---
+++ ERPProject.UI/Areas/Admin/Models/UserVM.cs
@@ -0,0 +1,30 @@
+﻿using ERPProject.Entity.DTO.DepartmentDTO;
+using ERPProject.Entity.DTO.RequestDTO;
+using ERPProject.Entity.Poco;
+
+namespace ERPProject.UI.Areas.Admin.Models
+{
+    public class UserVM
+    {
+        public long Id { get; set; }
+
+        public int DepartmentId { get; set; }
+
+        public int RolId { get; set; }
+
+        public string Name { get; set; } = null!;
+
+        public string LastName { get; set; } = null!;
+
+        public string Email { get; set; } = null!;
+
+        public string Phone { get; set; } = null!;
+
+        public string Password { get; set; } = null!;
+        public string DepartmentName { get; set; }
+        public virtual ICollection<RequestDTOResponse> Request { get; set; } = new List<RequestDTOResponse>();
+        public virtual ICollection<DepartmentDTOResponse> Department { get; set; } = new List<DepartmentDTOResponse>();
+        public virtual ICollection<Role> Role { get; set; } = new List<Role>();//DTO Response ile değiştir
+
+    }
+}
