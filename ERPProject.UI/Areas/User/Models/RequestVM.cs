34d1602a71dc9f1dde276d958cb6d6c0e9b496d7
enesozsecer <ensozscr@gmail.com>
2023-10-31 08:16:04 +0300

    UI'da vm ler eklendi.

---
+++ ERPProject.UI/Areas/User/Models/RequestVM.cs
@@ -0,0 +1,11 @@
+﻿using ERPProject.Entity.DTO.RequestDTO;
+using ERPProject.Entity.DTO.UserDTO;
+
+namespace ERPProject.UI.Areas.User.Models
+{
+    public class RequestVM
+    {
+        public virtual ICollection<RequestDTORequest> Requests { get; set; } = new List<RequestDTORequest>();
+        public virtual ICollection<UserDTORequest> Users { get; set; } = new List<UserDTORequest>();
+    }
+}
