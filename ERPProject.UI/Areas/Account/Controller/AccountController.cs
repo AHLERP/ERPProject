84dd76a00cfe2b37b3b132239075d4ffeed9aea1
Bilal <Bilal@DESKTOP-R04PVQ3>
2023-10-31 17:11:34 +0300

    UI işlemleri

---
+++ ERPProject.UI/Areas/Account/Controller/AccountController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ERPProject.UI.Areas.Account.Controller
+{
+    [Area("Account")]
+    public class AccountController : BaseController
+    {
+        private readonly string url = "https://localhost:7075/";
+        public AccountController(IHttpClientFactory httpClientFactory) : base(httpClientFactory)
+        {
+        }
+        [HttpGet("/Login")]
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
