32010b5527e268d7678ff8c3748c5122cdf34294
enesozsecer <ensozscr@gmail.com>
2023-11-08 08:59:24 +0300

    Invoice Entityye ekleme yapıldı. Invoice AdminLayouta Eklendi.

--- ERPProject.Entity/Poco/Invoice.cs
+++ ERPProject.Entity/Poco/Invoice.cs
@@ -7,6 +7,12 @@ namespace ERPProject.Entity.Poco;
 public partial class Invoice : BaseEntity
 {
     public int Id { get; set; }
+    public decimal Quantity { get; set; }
+    public decimal Price { get; set; }
+    public short QuantityUnit { get; set; }
+    public DateTime InvoiceDate { get; set; }
+    public string SupplierName { get; set; }
+    public string SupAddress { get; set; }
 
     public long OfferId { get; set; }
 
